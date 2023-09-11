2c8c4b788b60a304935b5cfad07ae39e1feea444
shijin65 <shijincht65@gmail.com>
2023-09-11 23:59:08 +0530

    new file

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,5 +1,24 @@
-const errorHandler =(err,req,res,next)=>{
-    const statucode = res.statucode ? req.statucode : 500;
-    res.json({message: err.message , stacktrace: err.stackt})
-}
+const errorHandler = (err, req, res, next) => {
+  const statucode = res.statucode ? req.statucode : 500;
+  switch (statucode) {
+    case 400:
+      res.json({
+        title: "validation failed",
+        message: err.message,
+        stacktrace: err.stackt,
+      });
+      break;
+      case 404:
+        res.json({
+            title: "not found",
+            message: err.message,
+            stacktrace: err.stackt,
+        });
+
+        break;
+    default:
+      break;
+  }
+  
+};
 module.exports = errorHandler;
