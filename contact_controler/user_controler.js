1372c48c13ae16536264812a48c730e131800cd5
shijin65 <shijincht65@gmail.com>
2023-09-13 17:11:31 +0530

    user login access token created

--- contact_controler/user_controler.js
+++ contact_controler/user_controler.js
@@ -1,76 +1,80 @@
-const asyncHanler = require("express-async-handler")
-const User = require("../model/user_model")
+const asyncHanler = require("express-async-handler");
+const User = require("../model/user_model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-
-
 //register user
 // /api/user/register
-const registeruser = asyncHanler(async(req,res)=>{
+const registeruser = asyncHanler(async (req, res) => {
+  const { username, email, password } = req.body;
 
-    const {username,email,password} = req.body;
+  if (!username || !email || !password) {
+    res.status(404);
+    throw new Error("all the fields a mantatory");
+  }
 
-            if (!username || ! email || !password) {
-                res.status(404);
-                throw new Error("all the fields a mantatory")
-            }
+  const useravailable = await User.findOne({ email });
 
-    const useravailable = await User.findOne({email});
+  if (useravailable) {
+    res.status(400);
+    throw new Error("user already exist");
+  }
 
-            if (useravailable) {
-                res.status(400);
-                throw new Error("user already exist");
-            }
+  const hashedpassword = await bcrypt.hash(password, 10);
+  console.log(hashedpassword);
 
-    const hashedpassword = await bcrypt.hash(password,10);
-    console.log(hashedpassword)
+  const user = await User.create({
+    username,
+    email,
+    password: hashedpassword,
+  });
+  console.log(`contact created : ${user}`);
+  if (user) {
+    res.status(201).json({ _id: user.id, email: user.email });
+  }
 
-    const user = await User.create({
-        username,
-        email,
-        password:hashedpassword,
-    })
-    console.log(`contact created : ${user}`)
-    if (user) {
-         res.status(201).json({_id : user.id , email : user.email});
-    }
-   
-    res.end();
+  res.end();
 });
-
-
-
-
 
 //login user
 // /api/user/login
-const loginuser = asyncHanler(async(req,res)=>{
-    const {email,password} = req.body;
-    if (! email || !password) {
-        res.status(404);
-        throw new Error("email and password are most")
-    }
+const loginuser = asyncHanler(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(404);
+    throw new Error("email and password are most");
+  }
 
-    const user = await User.find({email}) ;
-    console.log(user)
-    if(user && (await bcrypt.compare(password , user.password))) {
+  const user = await User.findOne({ email });
+  console.log(user);
+//   console.log(user.username);
 
-        const accesstoken = jwt.sign({
-            user
-        })
-        res.status(200).json(accesstoken);
-        res.end();
-    }
-
+  if (user && (await bcrypt.compare(password, user.password))) {
     
+    const accesstoken = jwt.sign(
+                            {
+                                user: {
+                                username: User.username,
+                                email: User.email,
+                                id: User.id,
+                                },
+                            },
+                                process.env.SECERT_ACCESSTOKEN,
+                            {   expiresIn: "1m" }
+    );
+    res.status(200).json( accesstoken );
+    
+  }else{
+    res.status(401);
+    throw new Error("auth error")
+  }
+  res.end();
 });
 
 // /api/user/current
-const currentuser = asyncHanler(async(req,res)=>{
-    res.status(200).json({title:"current user" });
-    res.end();
+const currentuser = asyncHanler(async (req, res) => {
+  res.status(200).json({ title: "current user" });
+  res.end();
 });
 
-
-module.exports = {registeruser,loginuser,currentuser};
+module.exports = { registeruser, loginuser, currentuser };
