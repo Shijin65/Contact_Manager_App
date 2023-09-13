a9d08a1f79e30704794bc1727e28d2de7c55799a
shijin65 <shijincht65@gmail.com>
2023-09-13 10:08:05 +0530

    model for the user is added

--- contact_controler/user_controler.js
+++ contact_controler/user_controler.js
@@ -3,19 +3,19 @@ const asyncHanler = require("express-async-handler")
 
 //register user
 
-// contact/api/user/register
+// /api/user/register
 const registeruser = asyncHanler(async(req,res)=>{
     res.status(200).json({title:"registration is available" });
     res.end();
 });
 
-// contact/api/user/login
+// /api/user/login
 const loginuser = asyncHanler(async(req,res)=>{
     res.status(200).json({title:"login user" });
     res.end();
 });
 
-// contact/api/user/current
+// /api/user/current
 const currentuser = asyncHanler(async(req,res)=>{
     res.status(200).json({title:"current user" });
     res.end();
