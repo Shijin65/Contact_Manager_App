4caa078c7f9fbd4190b18f9be264deae7eb5f578
shijin65 <shijincht65@gmail.com>
2023-09-19 18:01:03 +0530

    some changes are made

--- routes/user_route.js
+++ routes/user_route.js
@@ -5,7 +5,9 @@ const router = express.Router();
 
 
 router.post("/register", registeruser);
-
+router.get("/login",(req,res)=>{
+    res.send("this is a message for login user")
+})
 router.post("/login", loginuser);
 
 router.get("/current",validuser,currentuser);
--- server.js
+++ server.js
@@ -2,17 +2,25 @@ const express = require ("express");
 const errorHandler = require("./middleware/errorHandler");
 const connectDB = require("./Config/DBconnection");
 const dotenv = require("dotenv").config();
-
+// const cors = require("cors")
 
 connectDB();
 const app = express();
 const port=process.env.PORT;
+// const corsOptions = {
+//     Access:"http://localhost:8000",
+//     origin: 'http://localhost:8000/',
+//     methods: 'POST',
+//     allowedHeaders: 'Content-Type,Authorization',
+//     credentials: true, // Enable cookies and HTTP authentication headers
+//   };
 
 app.use(express.json());
+app.use(require("cors")());  
 app.use("/api/contact", require("./routes/contact_route"));
 app.use("/api/user", require("./routes/user_route"));
 app.use(errorHandler);
-app.use(require('cors')())
+
 
 app.listen(port,()=>{
     console.log(`the port is running at the port :${port}`)
