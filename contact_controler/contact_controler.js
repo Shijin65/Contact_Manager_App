544ae5b56f6ea78cc80bce291a52f6243e01ff1a
shijin p <shijincht65@gmail.com>
2023-09-23 19:15:18 +0530

    the list of new contacts after the deletion is sent to the client

--- contact_controler/contact_controler.js
+++ contact_controler/contact_controler.js
@@ -123,9 +123,12 @@ const deletecontact = asyncHanler(async (req,res)=>{
     }
     
     try {
-         await Contact.deleteOne({ _id: req.params.id });
-            res.status(400).json({"DELETED_CONTACT":contact.name});
-            res.end();
+        
+        await Contact.deleteOne({ _id: req.params.id });
+        const allContacts = await Contact.find({user_id : req.user.id});
+        // console.log("the user is ",req.user)
+        res.status(200).json({"DELETED_CONTACT":contact.name , newcont:allContacts});
+
     } catch (error) {
         console.log(error)
     }
